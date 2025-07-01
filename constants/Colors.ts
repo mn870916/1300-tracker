fe9ce6fe3d40550e4ff852202364f13ae366cf20
mn870916 <mn870916@gmail.com>
2025-07-01 16:07:50 +0800

    chore: colors constants

---
+++ constants/Colors.ts
@@ -0,0 +1,21 @@
+const tintColorLight = "#287799";
+const tintColorDark = "#fff";
+
+export const Colors = {
+  light: {
+    text: "#111827",
+    background: "#f8f8f8",
+    tint: tintColorLight,
+    icon: "#687076",
+    tabIconDefault: "#687076",
+    tabIconSelected: tintColorLight,
+  },
+  dark: {
+    text: "#ECEDEE",
+    background: "#151718",
+    tint: tintColorDark,
+    icon: "#9BA1A6",
+    tabIconDefault: "#9BA1A6",
+    tabIconSelected: tintColorDark,
+  },
+};
